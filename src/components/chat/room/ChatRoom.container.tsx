c945f691539b7b6cc71470033c996ff2058f6dc1
blissful-y0 <blissful.y0@gmail.com>
2021-07-27 20:14:20 +0900

    Update: Chat room structure

--- src/components/chat/room/ChatRoom.container.tsx
+++ src/components/chat/room/ChatRoom.container.tsx
@@ -8,8 +8,18 @@ export function Chat() {
     setMessages([
       {
         _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
+        text: "FDDSFDSF developer",
+        createdAt: "Tue Jul 27 2021 19:41:32 GMT+0900 (한국 표준시)",
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 1,
+        text: "HSADFSDFSADF developer",
+        createdAt: "Tue Jul 25 2021 19:41:32 GMT+0900 (한국 표준시)",
         user: {
           _id: 2,
           name: "React Native",
