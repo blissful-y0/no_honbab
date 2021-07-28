9aa8551ec505a9913f982139e63e5a533c1e75f5
blissful-y0 <blissful.y0@gmail.com>
2021-07-28 18:14:35 +0900

    Update: login auth flow added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect, useContext, useState, createContext } from "react";
 import "react-native-gesture-handler";
-import NavigationPage from "./pages/navigation";
+import AuthPage from "./pages/navigation";
+import LandingPage from "./pages/navigation/landing";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./src/apollo/client";
+import auth from "@react-native-firebase/auth";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./pages/navigation/tabNavigator";
+import Landing from "./pages/screens/";
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <NavigationPage />
-  </ApolloProvider>
-);
+export const AuthContext = createContext({
+  user: {},
+  setUser: (_: any) => {},
+});
+
+const App = () => {
+  const Stack = createStackNavigator();
+  const [user, setUser] = useState({});
+  const [initializing, setInitializing] = useState(true);
+
+  const onAuthStateChanged = (userCrendential) => {
+    setUser(userCrendential);
+    if (initializing) setInitializing(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {user ? (
+              <Stack.Screen name="tabNavigator" component={TabNavigator} />
+            ) : (
+              <Stack.Screen name="landing" component={Landing} />
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApolloProvider>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
--- pages/screens/mypage/index.tsx
+++ pages/screens/mypage/index.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Button, View } from "react-native";
+import auth from "@react-native-firebase/auth";
+import { AuthContext } from "../../../src/components/hoc/AuthProvider";
 
 const MyPage = () => {
-  return <></>;
+  const logOut = async () => {
+    try {
+      await auth().signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <View>
+        <Button title="hello" onPress={logOut}></Button>
+      </View>
+    </>
+  );
 };
 
 export default MyPage;
