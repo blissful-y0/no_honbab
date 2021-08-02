ed3d05bd12da0dbd81be7e323276e2706332f3e8
blissful-y0 <blissful.y0@gmail.com>
2021-08-02 22:13:09 +0900

    fix: meeting create state change

--- src/components/meeting/create/MeetingCreate.container.tsx
+++ src/components/meeting/create/MeetingCreate.container.tsx
@@ -11,11 +11,7 @@ import { useNavigation } from "@react-navigation/native";
 
 function Create() {
   const { user } = useContext(AuthContext);
-  const [createMeeting] = useMutation<IMutation, IMutationCreateMeetingArgs>(
-    CREATE_MEETING
-  );
   const navigation = useNavigation();
-
   const options = {
     weekday: "long",
     year: "numeric",
@@ -23,22 +19,32 @@ function Create() {
     day: "numeric",
   };
   const today = new Date();
+
+  const [createMeeting] = useMutation<IMutation, IMutationCreateMeetingArgs>(
+    CREATE_MEETING
+  );
+
+  const [input, setInput] = useState({
+    title: "",
+    dateLimit: 0,
+    date: today.toLocaleDateString("kor-KR"),
+    longitude: 0,
+    latitude: 0,
+    host: {
+      _id: user.id,
+      name: user.displayName,
+    },
+    recruitment: 0,
+    foodType: "",
+    contents: "",
+    address: "",
+    place: "",
+  });
+
   const [dateDifference, setDateDifference] = useState(0);
 
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
-  const [recuitNumber, setRecuitNumber] = useState(0);
-  const [foodType, setfoodType] = useState("음식 종류");
-
-  const [selectedDate, setSelectedDate] = useState(
-    today.toLocaleDateString("ko-KR")
-  );
   const [isPickerVisible, setPickerVisibility] = useState(false);
-
-  const [selectedLocation, setselectedLocation] =
-    useState("모임의 위치를 선택해 주세요");
   const [isPostboxVisible, setPostboxVisibility] = useState(false);
-  const [selectedPlace, setSelectedPlace] = useState("");
 
   const toggleDatePicker = () => {
     setPickerVisibility((prevState) => !prevState);
@@ -51,39 +57,18 @@ function Create() {
   const handleConfirm = (incomingDate) => {
     const date = incomingDate.toLocaleDateString("ko-KR", options);
     const time = incomingDate.toLocaleTimeString("ko-KR");
-    setSelectedDate(`${date} ${time}`);
+    setInput(...input, date: `${date} ${time}`,);
     setDateDifference(
       Math.ceil((incomingDate.getTime() - today.getTime()) / (1000 * 3600 * 24))
     );
     toggleDatePicker();
   };
 
-  const [input, setInput] = useState({
-    title: title,
-    dateLimit: dateDifference,
-    date: selectedDate,
-    longitude: 127.02761,
-    latitude: 37.498095,
-    host: {
-      //@ts-ignore
-      _id: user.uid,
-      //@ts-ignore
-      name: user.displayName,
-    },
-    recruitment: recuitNumber,
-    foodType: foodType,
-    contents: contents,
-    address: "서울특별시 강남구 테헤란로 3",
-    place: selectedPlace,
-  });
-
-  console.log(foodType);
-
   const onClickUploadButton = async () => {
     try {
       const result = await createMeeting({
         variables: {
-          createMeetingInput: input,
+          // createMeetingInput: input,
         },
       });
       //@ts-ignore
