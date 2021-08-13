66c34cb023b41b53de492d89a6a675334a5676ea
blissful-y0 <blissful.y0@gmail.com>
2021-08-13 23:03:32 +0900

    update: mypage UI fix

--- src/components/mypage/MyPage.container.tsx
+++ src/components/mypage/MyPage.container.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from "react";
 import auth from "@react-native-firebase/auth";
 import { AuthContext } from "../../../App";
 import MyPageUI from "./MyPage.presenter";
-import firestore from "@react-native-firebase/firestore";
 import { FETCH_JOINED_MEETING } from "./MyPage.query";
 import { useQuery } from "@apollo/client";
 import {
@@ -21,7 +20,7 @@ function MyPage() {
     FETCH_JOINED_MEETING,
     {
       variables: {
-        userId: user.uid,
+        userId: user?.uid,
       },
     }
   );
